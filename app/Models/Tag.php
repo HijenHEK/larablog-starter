984cd4a176dc9d6fdb51e85c908526ec373f1a63
HijenHek <k.hijen@gmail.com>
2020-10-25 17:02:49 +0100

    post and tag relations and setuo

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Tag extends Model
 {
     use HasFactory;
-    public function user(){
-        return $this->belongsTo(User::class) ;
-    }
+
+
     public function posts(){
-        return $this->belongsToMany(Tag::class) ;
+        return $this->belongsToMany(Post::class) ;
     }
 }
