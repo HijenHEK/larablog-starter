e010a5219584259016ee115057663498a1ea3c95
HijenHek <k.hijen@gmail.com>
2020-11-05 15:10:00 +0100

    header style and js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,28 @@
+window.$ = require('jquery');
 
-require('./bootstrap');
+
+toggleMenu = function()  {
+    if($('.drop-menu').hasClass('drop-active')) {
+        toggleDrop();
+    }
+    $('.nav-items').toggleClass('menu-active');
+    $('.toggler > .fa').toggleClass('fa-bars fa-times');
+
+}
+
+
+toggleDrop = function()  {
+    if($('.nav-items').hasClass('menu-active')) {
+        toggleMenu();
+    }
+    $('.drop-menu').toggleClass('drop-active');
+
+}
+
+
+
+$('main').on('click' , ()=>{
+    $('.nav-items').removeClass('menu-active');
+    $('.drop-menu').removeClass('drop-active');
+
+});
