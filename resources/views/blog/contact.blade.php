b9e21f8965301aa145a7ce617faeccb3a15f3113
HijenHek <k.hijen@gmail.com>
2020-11-09 06:25:24 +0100

    create form

--- resources/views/blog/contact.blade.php
+++ resources/views/blog/contact.blade.php
@@ -11,16 +11,16 @@
 
 <section class="contact-us">
 
-                        <div class="contact-section">
-                                <div class="head">
-                                    <h2>Send us a message</h2>
-                                </div>
+    <div class="contact-section">
+        <div class="head">
+            <h2>Send us a message</h2>
+        </div>
 
-                                <div class="content">
+        <div class="content">
 
 
-                                    {{-- testing notifications --}}
-                                    {{-- <form id="contact" action="/notif" method="post">
+            {{-- testing notifications --}}
+            {{-- <form id="contact" action="/notif" method="post">
                                                 @csrf
                                                 <div class="row">
 
@@ -34,89 +34,86 @@
                                             </form> --}}
 
 
-                                    <form id="contact" action="/contact" method="post">
-                                        @csrf
-                                        <div class="row">
-                                            <div class="group">
+            <form id="contact" action="/contact" method="post">
+                @csrf
+                <div class="row">
+                    <div class="group">
 
-                                                    <input name="name" class="control" type="text" id="name" placeholder="Your name"
-                                                        required="">
-                                                    @error('name')
-                                                    <p class="is-invalid">{{$message}}</p>
-                                                    @enderror
-
-                                            </div>
-                                            <div class="group">
-
-                                                    <input name="email" class="control" type="text" id="email" placeholder="Your email"
-                                                        required="">
-
-                                                @error('mail')
-                                                <p class="is-invalid">{{$message}}</p>
-                                                @enderror
-                                            </div>
-                                        </div>
-                                        <div class="group">
-
-                                                <input name="subject" class="control subj" type="text" id="subject" placeholder="Subject">
-                                                @error('subject')
-                                                <p class="is-invalid">{{$message}}</p>
-                                                @enderror
-
-                                        </div>
-                                        <div class="group">
-
-                                                <textarea name="message"      rows="6" id="message"
-                                                    placeholder="Your Message" required=""></textarea>
-                                                @error('message')
-                                                <p class="is-invalid">{{$message}}</p>
-                                                @enderror
-
-                                        </div>
-
-                                        <div class="group">
-
-                                                <button type="submit" id="form-submit" class="btn">Send
-                                                    Message</button>
-
-                                        </div>
-                                        @if (session('message'))
-                                        <div class="group">
-                                            <p class="is-valid">
-                                                {{session('message')}}
-                                            </p>
-                                        </div>
-                                        @endif
-                                </div>
-                                </form>
-                        </div>
-
-                    <div class="info-sidebar">
-                            <div class="head">
-                                <h2>contact information</h2>
-                            </div>
-                            <div class="content">
-                                <ul>
-                                    <li>
-                                        <span>PHONE NUMBER</span>
-
-                                        <span>090-484-8080</span>
-                                    </li>
-                                    <li>
-                                        <span>EMAIL ADDRESS</span>
-
-                                        <span>info@company.com</span>
-                                    </li>
-                                    <li>
-                                        <span>STREET ADDRESS</span>
-
-                                        <span>123 Aenean id posuere dui
-                                           , Praesent laoreet 10660</span>
-                                    </li>
-                                </ul>
-                            </div>
+                        <input name="name" class="control" type="text" id="name" placeholder="Your name" required="">
+                        @error('name')
+                        <p class="is-invalid">{{$message}}</p>
+                        @enderror
 
                     </div>
+                    <div class="group">
+
+                        <input name="email" class="control" type="text" id="email" placeholder="Your email" required="">
+
+                        @error('mail')
+                        <p class="is-invalid">{{$message}}</p>
+                        @enderror
+                    </div>
+                </div>
+                <div class="group">
+
+                    <input name="subject" class="control subj" type="text" id="subject" placeholder="Subject">
+                    @error('subject')
+                    <p class="is-invalid">{{$message}}</p>
+                    @enderror
+
+                </div>
+                <div class="group">
+
+                    <textarea name="message" rows="6" id="message" placeholder="Your Message" required=""></textarea>
+                    @error('message')
+                    <p class="is-invalid">{{$message}}</p>
+                    @enderror
+
+                </div>
+
+                <div class="group">
+
+                    <button type="submit" id="form-submit" class="btn">Send
+                        Message</button>
+
+                </div>
+                @if (session('message'))
+                <div class="group">
+                    <p class="is-valid">
+                        {{session('message')}}
+                    </p>
+                </div>
+                @endif
+        </div>
+        </form>
+    </div>
+
+    <div class="info-sidebar">
+        <div class="head">
+            <h2>contact information</h2>
+        </div>
+        <div class="content">
+            <ul>
+                <li>
+                    <span>PHONE NUMBER</span>
+
+                    <span>090-484-8080</span>
+                </li>
+                <li>
+                    <span>EMAIL ADDRESS</span>
+
+                    <span>info@company.com</span>
+                </li>
+                <li>
+                    <span>STREET ADDRESS</span>
+
+                    <span>123 Aenean id posuere dui
+                        , Praesent laoreet 10660</span>
+                </li>
+            </ul>
+        </div>
+
+    </div>
 
 
 
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -8,106 +8,61 @@
 'name' => '' ,
 'heading' => 'New Post ?'
 ])
+<div class="new-post-main">
+    <section class="new-post">
 
-    <section class="new-post-us">
-      <div class="container mt-5">
-        <div class="row">
+        {{-- <div class="head">
+            <h2>Create a post</h2>
+        </div> --}}
+        <div class="content">
+            <form id="newPost" action="/posts" method="POST">
+                @csrf
+                <div class=" group">
 
-          <div class="col-lg-12">
-            <div class="down-new-post">
-              <div class="row">
-                <div class="col-lg-12">
-                  <div class="sidebar-item new-post-form">
-                    <div class="sidebar-heading">
-                      <h2>Create a post</h2>
-                    </div>
-                    <div class="content">
-                      <form id="newPost" action="/posts"  method="POST">
-                        @csrf
-                        <div class="mt-5" >
-                          <div class=" form-group col-md-6 col-sm-12">
-                            <fieldset>
+                        <input name="title" class="control {{ $errors->first('title') ? 'is-invalid' : '' }}"
+                            type="text" id="title" value="{{old('title')}}">
+                    <p class="is-invalid">{{ $errors->first('title') }} </p>
 
-                            <input name="title" class="form-control {{ $errors->first('title') ? 'is-invalid' : '' }}" type="text" id="title" value="{{old('title')}}" >
-                            </fieldset>
-                            <p class="text-danger">{{ $errors->first('title') }} </p>
-
-                          </div>
-                          <div class=" form-group col-md-6 col-sm-12">
-                            <fieldset>
-                              <textarea name="body" class="form-control {{ $errors->first('body') ? 'is-invalid' : '' }}" cols="40" rows="10" value="{{old('body')}}" type="text" id="body" placeholder="write whats on your mind"  >
-                              </textarea>
-                            </fieldset>
-                            <p class="text-danger">{{ $errors->first('body') }} </p>
-
-                          </div>
-                          <div class=" form-group col-md-6 col-sm-12">
-                            <fieldset>
-                              <select name="tag[]"  data-live-search="true"
-                              class="form-control selectpicker {{ $errors->first('tag') ? 'is-invalid' : '' }}"
-                              id="tags" multiple >
-                                  @foreach ($tags as $tag)
-                                    <option value="{{$tag->id}}">
-                                      {{$tag->name}}
-                                    </option>
-                                  @endforeach
-
-                              </select>
-                            </fieldset>
-                            <p class="text-danger">{{ $errors->first('tag') }} </p>
-
-                          </div>
-                          <div class="col-lg-12">
-                            <fieldset>
-                              <button type="submit" id="form-submit" class="btn btn-success">Post it !</button>
-                            </fieldset>
-                          </div>
-
-                        </div>
-                      </form>
-                    </div>
-                  </div>
                 </div>
-                {{-- <div class="col-lg-4">
-                  <div class="sidebar-item contact-information">
-                    <div class="sidebar-heading">
-                      <h2>contact information</h2>
-                    </div>
-                    <div class="content">
-                      <ul>
-                        <li>
-                          <h5>090-484-8080</h5>
-                          <span>PHONE NUMBER</span>
-                        </li>
-                        <li>
-                          <h5>info@company.com</h5>
-                          <span>EMAIL ADDRESS</span>
-                        </li>
-                        <li>
-                          <h5>123 Aenean id posuere dui,
-                          	<br>Praesent laoreet 10660</h5>
-                          <span>STREET ADDRESS</span>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div> --}}
-              </div>
-            </div>
-          </div>
+                <div class=" group">
+                        <textarea name="body" class="control {{ $errors->first('body') ? 'is-invalid' : '' }}"
+                            cols="40" rows="10" value="{{old('body')}}" type="text" id="body"
+                            placeholder="write whats on your mind">
+                                  </textarea>
 
-          {{-- <div class="col-lg-12">
-            <div id="map">
-              <iframe src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" width="100%" height="450px" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
-          </div> --}}
+                    <p class="is-invalid">{{ $errors->first('body') }} </p>
 
+                </div>
+                <div class=" group">
+
+                        <select name="tag[]" data-live-search="true"
+                            class="control selectpicker {{ $errors->first('tag') ? 'is-invalid' : '' }}" id="tags"
+                            multiple>
+                            @foreach ($tags as $tag)
+                            <option value="{{$tag->id}}">
+                                {{$tag->name}}
+                            </option>
+                            @endforeach
+
+                        </select>
+
+                    <p class="is-invalid">{{ $errors->first('tag') }} </p>
+
+                </div>
+                <div class="group">
+
+                        <button type="submit" id="submit" class="btn">Post it !</button>
+
+                </div>
+            </form>
         </div>
-      </div>
+
     </section>
 
+</div>
 
-    @include('layouts.partials.footer')
+
+@include('layouts.partials.footer')
 
 
 
