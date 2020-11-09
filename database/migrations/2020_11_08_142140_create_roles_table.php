b9fd8caec23c5a6227df126c85c62ec0c4871a2f
HijenHek <k.hijen@gmail.com>
2020-11-09 20:15:17 +0100

    removing users pivot

--- database/migrations/2020_11_08_142140_create_roles_table.php
+++ database/migrations/2020_11_08_142140_create_roles_table.php
@@ -39,16 +39,16 @@ class CreateRolesTable extends Migration
             $table->foreign('ability_id')->references('id')->on('abilities')->onDelete('Cascade');
 
         });
-        Schema::create('role_user', function (Blueprint $table) {
-            //
-            $table->primary(['role_id','user_id']);
-            $table->unsignedBigInteger('user_id') ;
-            $table->unsignedBigInteger('role_id');
+        // Schema::create('role_user', function (Blueprint $table) {
+        //     //
+        //     $table->primary(['role_id','user_id']);
+        //     $table->unsignedBigInteger('user_id') ;
+        //     $table->unsignedBigInteger('role_id');
 
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        //     $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        //     $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
-        });
+        // });
     }
 
     /**
