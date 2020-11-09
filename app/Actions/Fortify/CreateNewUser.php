b32c314e3b045631c0ab4c698ff3ba4fb56a9b6e
HijenHek <k.hijen@gmail.com>
2020-11-09 20:16:30 +0100

    customizing fortify reg + role_id

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -20,6 +20,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input)
     {
+
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => [
@@ -31,12 +32,17 @@ class CreateNewUser implements CreatesNewUsers
             ],
             'password' => $this->passwordRules(),
         ])->validate();
+
         $user = User::create([
                 'name' => $input['name'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
             ]);
-        $user->assignRole('guest');
+            $user->assignRole('Guest');
+
+        if( $user->id == 1 ) {
+            $user->assignRole('Admin');
+        }
         return $user ;
     }
 }
